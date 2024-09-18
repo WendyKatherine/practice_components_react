8138198125173fe413c40ee246981fb69bd9c7fc
Wendy Katherine Gomez <43182244+WendyKatherine@users.noreply.github.com>
2024-09-17 20:08:33 -0500

    my first component

---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const MyFirstComponent = () => {
+  return (
+    <>
+        <div>My First component</div>
+        <h1>Hola bebes estoy aprendiendo por fin react </h1>
+        <ul>
+            <li>Componentes</li>
+            <li>Eventos</li>
+            <li>Estados del Hooks</li>
+            <li>Props</li>
+        </ul>
+    </>
+  )
+}
+
+export default MyFirstComponent
